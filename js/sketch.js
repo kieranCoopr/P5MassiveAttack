d5c19c9fa86be3bf477aad63c4d7e6b14cc6830e
kieranCoopr <93775394+kieranCoopr@users.noreply.github.com>
2021-12-10 16:53:39 +0000

    Added the visuals for massive effect.

--- js/sketch.js
+++ js/sketch.js
@@ -1,11 +1,59 @@
+var mySong, amplitude, play;
+
+function preload(){
+    mySong = loadSound('assets/massiveAttackAngel 2.mp3');
+}
+
 function setup() {
-    var canvas = createCanvas(800, 800);
+    var canvas = createCanvas(window.innerWidth, window.innerHeight);
     canvas.parent("p5container");
+
+    rectMode(CENTER);
+
+    amplitude = new p5.Amplitude();
+    play= false;
+    button = createButton("Play")
+    button.center(); 
+    button.addClass('btn');   
+    button.mousePressed(()=>{
+        play = true;
+        mySong.play();
+        button.hide();
+        
+
+    });
+
 }
 
 function draw() {
-    background(220);
 
-    fill(255, 255, 255);
-    ellipse(400, 400, 200);
+    console.log(play)
+    if(play == true){
+         //console.log(amplitude.getlevel())
+    var level = amplitude.getLevel();
+    var vol = map(level,0, 1, 0, 1000);
+    var col = map(level,0, 1, 0, 225);
+
+    background(col);
+
+    //console.log(vol)
+    noStroke()
+
+    if(vol>200){
+        console.log('kick peak');
+        fill(col+10);
+    ellipse(width/2, height/2, vol + 100);
+    }
+    
+
+    fill(col, 255, 255);
+    ellipse(width/2, height/2, vol);
+
+    fill(100, col, 255);
+    ellipse(width/2, height/2, vol/2);
+        
+
+    }
+   
+
 }
